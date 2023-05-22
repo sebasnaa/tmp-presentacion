6ce679f617ae59056c7a3994c07779c0f11bc777
sebasnaa <wsntsebastian@gmail.com>
2023-05-22 14:18:20 +0200

    who has two thumbs and is a genius? not this guy!

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 
+# gh workflow list -a | awk '{print $1}'
 gh workflow list -a
 
-gh workflow disable "1-Push-Event"
+
+gh workflow disable "1-Pull-request-Event"
+gh workflow disable "2-Jobs-Encadenados"
+gh workflow disable "3-Variables-Context"
+gh workflow disable "4-Push-Using-Caching"
+gh workflow disable "4-Push-No-Using-Caching"
 gh workflow disable "1-Issues-Event"
+gh workflow disable "1-Push-Event"
+
+
 
 if [ $# -gt 0 ]
   then
