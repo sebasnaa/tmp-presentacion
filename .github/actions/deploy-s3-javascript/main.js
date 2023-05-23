0196dfe680ada3cf8f7b3721ce324fe7125305ab
sebasnaa <wsntsebastian@gmail.com>
2023-05-23 10:00:31 +0200

    Reticulating splines...

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,24 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+// Give some github context values
+
 const exec = require('@actions/exec')
 
 function run() {
+
+    // 1. Get input values
+    const bucket = core.getInput('bucket', {required: true})
+    const bucketRegion = core.getInput('bucket-region' ,{required: true})
+    const distFolder = core.getInput('dist-folder')
+
+
+    // 2. Upload files
+    // AWS CLi is already in the runner
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
+
+
     core.notice('Hello from custom JS')
 }
 
